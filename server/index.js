76f514f2d20551d5df75e3482eee291d50d28580
noearobles <roblesnoe1030@gmail.com>
2023-04-04 03:53:23 -0500

    modified css

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ function init() {
 init()
 let dishes = [];
 const poke_card = document.getElementById("pokemonBox")
-
+const generateList = (arg) => {
+    let items = "";
+    for (let i = 0; i < arg.length; i++) {
+        items += `<li>${arg[i]}</li>`
+    }
+    return items
+}
 
 const search = document.querySelector("#pokemonName");
 const input = document.getElementById("form");
@@ -38,78 +44,78 @@ let dishArr = [
         "Miso Soup",
         "misoSoup.jpeg",
         6,
-        [
-            ["Tofu", "Wakame", "Scallions"],
 
-        ],
+        ["Tofu", "Wakame", "Scallions"],
+
+        ,
         "Vegan", "Not GF"
     ], [
         "Charred Edamame",
         "charredEdamame.jpeg",
         11,
-        [
-            ["Smoked Edamame", "Chili Honey Furikake", "Togarashi"],
 
-        ],
+        ["Smoked Edamame", "Chili Honey Furikake", "Togarashi"],
+
+        ,
         "Vegan", "GF"],
     [
         "Cucumber Sunomono",
         "cucumberSunomono.jpeg",
         9,
-        [
-            ["Cucumber", "Daikon Radish", "Sesame", "Shiso", "White Onion Rice Vinegar"],
 
-        ],
+        ["Cucumber", "Daikon Radish", "Sesame", "Shiso", "White Onion Rice Vinegar"],
+
+        ,
         "Vegan", "GF"
     ], ["Seasonal Vegetable Tempura",
         "vegTempura.jpeg",
         14,
-        [
-            ["Ginger Kaeshi", "Tempura Fried Carrots", "Egg Plant", "Mushroom", "Squah", "Onion", "Bell Pepper"],
 
-        ],
+        ["Ginger Kaeshi", "Tempura Fried Carrots", "Egg Plant", "Mushroom", "Squah", "Onion", "Bell Pepper"],
+
+        ,
         "", "Not GF"], ["Blistered Shishito Peppers",
         "shishitoPeppers.jpeg",
         8,
-        [
-            ["Blistered Peppers", "Green Yuzukosho", "Ponzu Sauce"],
 
-        ],
+        ["Blistered Peppers", "Green Yuzukosho", "Ponzu Sauce"],
+
+        ,
         "", ""], ["Chicken Karaage",
         "karaage.jpeg",
         15,
-        [
-            ["Fried Chicken Thighs", "Nori Honey", "Togarashi", "Kewpie Mayo"],
 
-        ],
+        ["Fried Chicken Thighs", "Nori Honey", "Togarashi", "Kewpie Mayo"],
+
+        ,
         "", ""], ["Crispy Baby Octopus",
         "octopus.jpeg",
         18,
-        [
-            ["Takoyaki", "Bonito Flakes", "Kewpie Mayo"],
 
-        ],
+        ["Takoyaki", "Bonito Flakes", "Kewpie Mayo"],
+
+        ,
         "", ""], ["Gyoza Tacos",
         "tacos.jpeg",
         18,
-        [
-            ["A5 or Tuna", "Aji Amarillo"]
 
-        ],
+        ["A5 or Tuna", "Aji Amarillo"]
+
+        ,
         "", "Not GF"], ["Mushroom Ceviche",
         "ceviche.jpeg",
         14,
-        [
-            ["Aji Amarillo", "Lime", "Cilantro","Avocado","Serano","Japanese Mushrooms (mytahkis bunashimeji)"],
 
-        ],
+        ["Aji Amarillo", "Lime", "Cilantro", "Avocado", "Serano", "Japanese Mushrooms (mytahkis bunashimeji)"],
+
+        ,
         "", ""], ["Brussel Sprouts",
         "brussels.jpeg",
         10,
-        [
-            ["Fried Brussels", "Nato Glaze", "Pickled Root Vegetable","Togorashi"],
 
-        ],
+        ["Fried Brussels", "Nato Glaze", "Pickled Root Vegetable", "Togorashi"],
+
+        ,
         "", ""]]
 
 
@@ -139,9 +145,9 @@ function getAllDishes() {
     <div>
     <small class="type"><span>${gluten}</span></small>
     </div>
-    <div>
-    <small class="stats"><span>Ingredients:<b> ${ingredients}<b></span></small>
-    </div>`;
+    <ol>
+    <small class="stats">Ingredients:<b> ${generateList(ingredients)}<b></small>
+    </ol>`;
         pMonEl.innerHTML = pokeInnerHTML;
         poke_card.appendChild(pMonEl);
     })
